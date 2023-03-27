20ed329f5bfac0870e0c43ac7bf4b4ea3014ce7a
Lucas BJ <lucasbrjacob@outlook.com>
2023-03-27 19:35:23 -0300

    WIP

--- app/src/main/java/com/example/fundatecheroes/SplashScreenActivity.kt
+++ app/src/main/java/com/example/fundatecheroes/SplashScreenActivity.kt
@@ -3,6 +3,7 @@ package com.example.fundatecheroes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.fundatecheroes.login.view.LoginActivity
 import com.example.fundatecheroes.profile.view.ProfileActivity
 
@@ -10,7 +11,9 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        startActivity(Intent(this@SplashScreenActivity, LoginActivity::class.java))
-        finish()
+        Handler().postDelayed( Runnable() {
+            startActivity(Intent(this@SplashScreenActivity, LoginActivity::class.java))
+            finish()
+        }, 1000);
     }
 }
